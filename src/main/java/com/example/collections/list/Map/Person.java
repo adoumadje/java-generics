78c4730d5a327a9bb6fc3cbb2acb61f05de8c480
adoumadje <adoumadjecaleb@gmail.com>
2025-07-10 14:05:32 +0200

    Maps and Sets

---
+++ src/main/java/com/example/collections/list/Map/Person.java
@@ -0,0 +1,19 @@
+package com.example.collections.list.Map;
+
+import java.util.Objects;
+
+public class Person {
+    private String name;
+    private int age;
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Person person)) return false;
+        return age == person.age && Objects.equals(name, person.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+}
